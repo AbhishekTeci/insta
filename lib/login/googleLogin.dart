2db27fd6b07e326e185a092187c9f22412bad32f
Abhishek <abhishekkumar833337@gmail.com>
2023-04-03 18:18:39 +0530

    logintoGoogle

--- lib/login/googleLogin.dart
+++ lib/login/googleLogin.dart
@@ -1,48 +1,46 @@
 
+import 'package:google_sign_in/google_sign_in.dart';
 
-// import 'package:google_sign_in/google_sign_in.dart';
-//
-// final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
-//
-// class GoogleLogin {
-//  static GoogleSignInAccount? currentUser;
-//
-//   void initState() {
-//     _googleSignIn.onCurrentUserChanged.listen((account) {
-//         currentUser = account;
-//       _googleSignIn.signInSilently();
-//     });
-//   }
-//
-//
-//   static googleLogin(){
-//     GoogleSignInAccount? user = currentUser;
-//
-//     if(user != null){
-//       SignIn();
-//      // Navigator.pushNamed(context, 'signup');
-//
-//     }else{
-//       SignOut();
-//     }
-//
-//
-//   }
-//
-// }
-//
-// void SignOut(){
-//   _googleSignIn.disconnect();
-// }
-//
-//
-//
-//
-// Future<void> SignIn() async {
-//   try{
-//     await _googleSignIn.signIn();
-//   }catch(e){
-//     e;
-//   }
-//
-// }
+final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+class GoogleLogin {
+ static GoogleSignInAccount? currentUser;
+
+  void initState() {
+    _googleSignIn.onCurrentUserChanged.listen((account) {
+        currentUser = account;
+      _googleSignIn.signInSilently();
+    });
+  }
+
+
+  static googleLogin(){
+    GoogleSignInAccount? user = currentUser;
+
+    if(user != null){
+      SignIn();
+
+    }else{
+      SignOut();
+    }
+
+
+  }
+
+}
+
+void SignOut(){
+  _googleSignIn.disconnect();
+}
+
+
+
+
+Future<void> SignIn() async {
+  try{
+    await _googleSignIn.signIn();
+  }catch(e){
+    e;
+  }
+
+}
